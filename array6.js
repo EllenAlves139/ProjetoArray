c3e6b6b7101be8c991b76753e247c3057f257780
gabriel alejandro e kayna rodrigues <0000941456@senaimgaluno.com.br>
2025-06-04 09:23:55 -0300

    segundo commit

---
+++ array6.js
@@ -0,0 +1,7 @@
+let produtos = ['Placa Mãe' , 'Processador' , 'Memória RAM' , 'SDD'];
+produtos.push('Placa de Video' , 'Fonte'); /*Adiciona 'Placa de Video' e 'Fonte' no fim*/
+
+
+
+console.log("após push():",produtos);
+//Saida:['Placa Mãe','processador','Memória RAM','SDD','Placa deVideo','Fonte']
